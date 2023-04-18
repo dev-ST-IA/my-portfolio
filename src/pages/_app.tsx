3eaf33ff27403e7f121d933c20692104a2c22d8b
Salman Thasleem <salmanthasleem@gmail.com>
2023-04-18 07:17:51 +0530

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { ApolloProvider } from '@apollo/client'
+import { client } from '@/lib/apolloClient'
+import ContentfulProvider from '@/context/ContentfulContext'
+import { ThemeProviderWrap } from '@/styles/theme'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ThemeProviderWrap>
+      <ApolloProvider client={client}>
+        <ContentfulProvider>
+          <Component {...pageProps} />
+        </ContentfulProvider>
+      </ApolloProvider>
+    </ThemeProviderWrap>
+  )
 }
